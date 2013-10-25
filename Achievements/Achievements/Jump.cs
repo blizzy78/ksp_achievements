6f1bf3b7e02c3deda95a6ad8c41c25763797ebe9
Maik Schreiber <blizzy@blizzy.de>
2013-10-25 19:53:53 +0200

    add "Space Dunk"

---
+++ Achievements/Achievements/Jump.cs
@@ -0,0 +1,83 @@
+﻿/*
+Achievements - Brings achievements to Kerbal Space Program.
+Copyright (C) 2013 Maik Schreiber
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+class JumpFactory : AchievementFactory {
+	public IEnumerable<Achievement> getAchievements() {
+		return new Achievement[] {
+			new SpaceDunk()
+		};
+	}
+
+	public Category getCategory() {
+		return Category.GROUND_OPERATIONS;
+	}
+}
+
+class SpaceDunk : AchievementBase {
+	private const double MIN_HEIGHT = 240;
+
+	private bool onSurfaceStep;
+	private double startAltitude;
+	private float startFuel;
+	private Body startBody;
+
+	private void onVesselChange(Vessel vessel) {
+		onSurfaceStep = false;
+	}
+
+	public override bool check(Vessel vessel) {
+		if ((vessel != null) && vessel.isEVA()) {
+			if (!onSurfaceStep) {
+				onSurfaceStep = vessel.isOnSurface();
+			}
+
+			if (vessel.isOnSurface()) {
+				startAltitude = vessel.altitude;
+				startFuel = getFuel(vessel);
+				startBody = vessel.getCurrentBody();
+			}
+
+			return onSurfaceStep && !vessel.isOnSurface() &&
+				((vessel.altitude - startAltitude) >= MIN_HEIGHT) && (getFuel(vessel) == startFuel) &&
+				vessel.getCurrentBody().Equals(startBody);
+		} else {
+			return false;
+		}
+	}
+
+	private float getFuel(Vessel vessel) {
+		return vessel.FindPartModulesImplementing<KerbalEVA>().First().Fuel;
+	}
+
+	public override string getTitle() {
+		return "Space Dunk";
+	}
+
+	public override string getText() {
+		return "Jump " + MIN_HEIGHT.ToString("F0") + " m high above the surface of a celestial body without using the jet pack.";
+	}
+
+	public override string getKey() {
+		return "eva.jump." + MIN_HEIGHT.ToString("F0");
+	}
+}
