a6923be78f4f7a7cb2f3155341dd2889627da45c
Maik Schreiber <blizzy@blizzy.de>
2014-01-06 19:23:14 +0100

    update copyright years

--- Achievements/AchievementsWindow.cs
+++ Achievements/AchievementsWindow.cs
@@ -1,6 +1,6 @@
 ﻿/*
 Achievements - Brings achievements to Kerbal Space Program.
-Copyright (C) 2013 Maik Schreiber
+Copyright (C) 2013-2014 Maik Schreiber
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
